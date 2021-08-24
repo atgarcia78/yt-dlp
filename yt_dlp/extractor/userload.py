9c189ef98e9b1c52d8e751619d66bf1b9c8d26c0
atgarcia78 <a.tgarc@gmail.com>
2021-08-24 04:49:25 +0200

    upt

--- yt_dlp/extractor/userload.py
+++ yt_dlp/extractor/userload.py
@@ -1,97 +1,140 @@
 from __future__ import unicode_literals
 
 
-import re
+
 from .common import InfoExtractor, ExtractorError
 from ..utils import (
-    urlencode_postdata,
+    
     sanitize_filename,
-    std_headers,
     int_or_none
 
 )
 import httpx
-import brotli
+
+
+from selenium.webdriver import Firefox, FirefoxProfile
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By
+
+import time
+
+import traceback
+import sys
+
 
 class UserLoadIE(InfoExtractor):
 
     IE_NAME = 'userload'
-    _VALID_URL = r'https?://(?:www\.)?userload\.co'
+    _VALID_URL = r'https?://(?:www\.)?userload\.co/(?:embed|e|f)/(?P<id>[^\/$]+)(?:\/|$)'
+    
+    _FF_PROF = ['/Users/antoniotorres/Library/Application Support/Firefox/Profiles/cs2cluq5.selenium5_sin_proxy',
+                '/Users/antoniotorres/Library/Application Support/Firefox/Profiles/7mt9y40a.selenium4',
+                '/Users/antoniotorres/Library/Application Support/Firefox/Profiles/yhlzl1xp.selenium3',
+                '/Users/antoniotorres/Library/Application Support/Firefox/Profiles/wajv55x1.selenium2',
+                '/Users/antoniotorres/Library/Application Support/Firefox/Profiles/xxy6gx94.selenium',
+                '/Users/antoniotorres/Library/Application Support/Firefox/Profiles/0khfuzdw.selenium0']
+
+ 
+
+
+    def wait_until(self, _driver, time, method):
+        try:
+            el = WebDriverWait(_driver, time).until(method)
+        except Exception as e:
+            el = None
+            
+        return el  
+    
+    def _get_filesize(self, url):
+        
+        count = 0
+        try:
+            
+            _res = None
+            while (count<3):
+                
+                try:
+                    
+                    res = httpx.head(url)
+                    if res.status_code > 400:
+                        time.sleep(1)
+                        count += 1
+                    else: 
+                        _res = int_or_none(res.headers.get('content-length')) 
+                        break
+            
+                except Exception as e:
+                    count += 1
+        except Exception as e:
+            pass
+
+        
+        return _res
 
     
     def _real_extract(self, url):
         
    
-        client = httpx.Client()
-        headers = std_headers
-        headers.update({"Alt-Used": "userload.co", "Referer": "https://www.myvidster.com"})
-        
-        res  = client.get(url, headers = headers)
-        
-        if res.headers.get("content-encoding") == "br":
-            webpage = (brotli.decompress(res.content)).decode("UTF-8", "replace")
-        else: webpage = res.text
-        
-        #self.to_screen(webpage)      
-        
-        data = re.findall(r"var\|+([^\']*)\'", webpage.replace(" ",""))
-        _data = None
-        if data:
-            _data = data[0].split('|')
-        
-        if not _data:
-            raise ExtractorError("No video data")
-        
-        #self.to_screen(data)
-        
-        data = {
-            "morocco": _data[13],
-            "mycountry": _data[17],
-        }
-        
-        self.to_screen(_data[13])
-        self.to_screen(_data[17])
-        
-        headers_post = std_headers
-        headers_post.update({
-                "Referer": url,
-                "Origin": "https://userload.co",
-                "Content-Type": "application/x-www-form-urlencoded",
-                "Accept": "*/*",
-                "Alt-Used": "userload.co"
-            })
-
-        res = client.post(
-            "https://userload.co/api/request/",
-            data=data,
-            headers=headers_post
-        )
-        self.to_screen(f"{res}:{res.request.headers}:{res.headers}")
-        if res.headers.get("content-encoding") == "br":
-            video_info = (brotli.decompress(res.content)).decode("UTF-8", "replace")
-        else: video_info = res.text
-        
-        
-        #self.to_screen(video_info)
-        
-        if not video_info or not video_info.startswith("http"):
-            raise ExtractorError("No video data after api request")
+        self.report_extraction(url)
+        prof = self._FF_PROF.pop()
+        self._FF_PROF.insert(0, prof)
             
-        format_video = {
-            'format_id' : 'http-mp4',
-            'url' : video_info,
-            'filesize' : int_or_none(client.head(video_info).headers.get('content-length')),
-            'ext' : 'mp4'
-        }
-
-        entry_video = {
+        opts = Options()
+        opts.headless = True
             
-            'id' : _data[0],
-            'title' : _data[3],
-            'formats': [format_video],
-            'ext': "mp4"
-        }
+        try:                            
+                            
+            driver = Firefox(firefox_binary="/Applications/Firefox Nightly.app/Contents/MacOS/firefox", options=opts, firefox_profile=FirefoxProfile(prof))
+ 
+            self.to_screen(f"ffprof[{prof}]")
+            
+            driver.set_window_size(1920,575)
+            
+            _url = url.replace('/e/', '/f/').replace('/embed/', '/f/')
+            
+            driver.get(_url)
+            el = self.wait_until(driver, 60, ec.presence_of_element_located((By.ID,"videooverlay")))
+            if el: el.click()
+            res = driver.find_elements_by_id("olvideo_html5_api")
+            if not res: raise ExtractorError("no info")
+            video_url = res[0].get_attribute("src")
+            if not video_url: raise ExtractorError("no video url") 
+            
+            title = driver.title.replace(" | userload","").replace(".mp4","").strip()
+            videoid = self._match_id(url)
+            
+            _entry_video = None
+            
+            _format = {
+                    'format_id': 'http-mp4',
+                    'url': video_url,
+                    'filesize': self._get_filesize(video_url),
+                    'ext': 'mp4'
+            }
+            
+            _entry_video = {
+                'id' : videoid,
+                'title' : sanitize_filename(title, restricted=True),
+                'formats' : [_format],
+                'ext': 'mp4'
+            }   
+            
+        except Exception as e:
+            lines = traceback.format_exception(*sys.exc_info())
+            self.to_screen(f"{repr(e)} {str(e)} \n{'!!'.join(lines)}")
+            if "ExtractorError" in str(e.__class__): raise
+            else: raise ExtractorError(str(e))
+        finally:
+            driver.quit()
+        
+        if not _entry_video: raise ExtractorError("no video info")
+        else:
+            return _entry_video    
 
-        return entry_video
+
+
+       
 
 
