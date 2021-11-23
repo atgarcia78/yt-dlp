4cfcdedab2dde3e53dab882a8afdad9f9556fb32
atgarcia78 <a.tgarc@gmail.com>
2021-11-23 18:16:05 +0100

    upt

--- yt_dlp/extractor/gaybeeg.py
+++ yt_dlp/extractor/gaybeeg.py
@@ -1,1 +1,254 @@
-404: Not Found
+from __future__ import unicode_literals
+
+import re
+
+
+from ..utils import ExtractorError
+
+from .webdriver import SeleniumInfoExtractor
+
+from concurrent.futures import (
+    ThreadPoolExecutor   
+)
+
+
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By
+
+
+from .netdna import NetDNAIE
+
+import logging
+import traceback
+import sys
+
+from datetime import datetime
+
+
+class GayBeegBaseIE(SeleniumInfoExtractor):
+    
+    
+    @staticmethod
+    def _get_entries_netdna(el_list):
+        
+        _list_urls_netdna = {}
+        _next = None
+        for _el in el_list:
+            _url = _el.get_attribute('href')
+            _tree = _el.find_elements(by=By.XPATH, value="./ancestor-or-self::*")
+            _tree.reverse()
+            if _next:
+                _list_urls_netdna[_url] = {'text': _next}
+                _next = None
+            else:
+                if any((_res:=el.text) for el in _tree):
+            
+                    _text = _res.splitlines()
+                    _list_urls_netdna[_url] = {'text': _text[0]}
+                    if len(_text) == 2 and any(_ext in _text[1] for _ext in ('mp4', 'zip')):
+                        _next = _text[1]
+            if any((_el_date:=el.find_elements(by=By.CLASS_NAME, value='date')) for el in _tree):
+                    _date = _el_date[0].text
+                    _list_urls_netdna[_url].update({'date': _date})
+                    
+                        
+         
+        
+        entries = []
+        
+        for _url, _item in _list_urls_netdna.items():
+            
+            _info_video = NetDNAIE.get_video_info(_item.get('text'))
+            _info_date = datetime. strptime(_item.get('date'), '%B %d, %Y')
+            entries.append({'_type' : 'url', 'url' : _url, 'ie' : 'NetDNA', 'title': _info_video.get('title'), 'id' : _info_video.get('id'), 'ext': _info_video.get('ext'), 'filesize': _info_video.get('filesize'), 'release_date': _info_date.strftime('%Y%m%d'), 'release_timestamp': int(_info_date.timestamp())})
+        
+                                    
+        return entries
+    
+    @staticmethod
+    def _get_entries_gaybeeg(el_list):
+        entries = [{'_type' : 'url', 'url' : _url, 'ie' : 'GayBeeg'}
+                        for el in el_list
+                                    for el_tagh2 in el.find_elements(by=By.TAG_NAME, value="h2")
+                                        for el_taga in el_tagh2.find_elements(by=By.TAG_NAME, value="a")
+                                            if "//gaybeeg.info" in (_url:=el_taga.get_attribute('href'))]    
+        return entries
+    
+ 
+    
+    
+    def _get_entries(self, url, driver=None):
+        
+        try:
+        
+            if not driver:
+                _keep = False
+                driver = self.get_driver()
+            else:
+                _keep = True
+                        
+            
+            driver.get(url)
+            
+            SCROLL_PAUSE_TIME = 2
+
+            last_height = driver.execute_script("return document.body.scrollHeight")
+
+            while True:
+                # Scroll down to bottom
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+                # Wait to load page
+                self.wait_until(driver, SCROLL_PAUSE_TIME, ec.title_is("DUMMYFORWAIT"))
+               
+                # Calculate new scroll height and compare with last scroll height
+                new_height = driver.execute_script("return document.body.scrollHeight")
+                if new_height == last_height:
+                    break
+                last_height = new_height
+        
+            
+            
+            el_a_list = self.wait_until(driver, 60, ec.presence_of_all_elements_located((By.XPATH, '//a[contains(@href, "//netdna-storage.com")]')))
+            
+            if el_a_list:
+                
+                entries = GayBeegBaseIE._get_entries_netdna(el_a_list)
+            
+                return entries
+            
+        finally:
+            if not _keep: 
+                try:
+                    self.rm_driver(driver)
+                except Exception:
+                    pass
+            
+            
+        
+                
+
+
+class GayBeegPlaylistPageIE(GayBeegBaseIE):
+    IE_NAME = "gaybeeg:onepage:playlist"
+    _VALID_URL = r'https?://(www\.)?gaybeeg\.info.*/page/.*'
+    
+
+    def _real_extract(self, url):        
+        
+        try:
+                   
+            self.report_extraction(url)
+            
+            entries = self._get_entries(url)
+            
+            if not entries: raise ExtractorError("No entries")  
+                      
+            return {
+                '_type': "playlist",
+                'id': "gaybeeg",
+                'title': "gaybeeg",
+                'entries': entries
+            }          
+   
+            
+        except ExtractorError as e:
+            raise
+        except Exception as e:
+            self.to_screen(repr(e))            
+            raise 
+
+class GayBeegPlaylistIE(GayBeegBaseIE):
+    IE_NAME = "gaybeeg:allpages:playlist"    
+    _VALID_URL = r'https?://(www\.)?gaybeeg\.info/(?:((?P<type>(?:site|pornstar|tag))(?:$|(/(?P<name>[^\/$\?]+)))(?:$|/$|/(?P<search1>\?(?:tag|s)=[^$]+)$))|((?P<search2>\?(?:tag|s)=[^$]+)$))'
+    
+    
+    def _real_extract(self, url):        
+        
+        try:
+                                   
+            self.report_extraction(url)
+            
+            driver = self.get_driver()
+            driver.get(url)
+            
+            
+            el_pages = self.wait_until(driver, 15, ec.presence_of_all_elements_located((By.CLASS_NAME, "pages")))
+            
+            if el_pages:
+                
+                num_pages = int(el_pages[0].get_attribute('innerHTML').split(' ')[-1])
+                self.to_screen(f"Pages to check: {num_pages}")
+                el_page = self.wait_until(driver, 30, ec.presence_of_element_located((By.CLASS_NAME, "page")))
+                _href = el_page.get_attribute('href')
+                list_urls_pages = [re.sub('page/\d+/', f'page/{i}/', _href) for i in range(1, num_pages+1)]
+                
+                self.to_screen(list_urls_pages)
+                
+                self.rm_driver(driver)
+
+                _num_workers = min(4, len(list_urls_pages))
+                
+                with ThreadPoolExecutor(thread_name_prefix="gaybeeg", max_workers=_num_workers) as ex:
+                    futures = [ex.submit(self._get_entries, _url) for _url in list_urls_pages] 
+            
+                
+                entries = []
+                for d in futures:
+                    try:
+                        entries += d.result()
+                    except Exception as e:
+                        lines = traceback.format_exception(*sys.exc_info())
+                        self.to_screen(f'{type(e)} \n{"!!".join(lines)}')  
+                        raise ExtractorError(str(e)) from e
+                        
+            else: #single page
+                self.to_screen("Single page, lets get entries")
+                entries = self._get_entries(url, driver)
+                                       
+            if entries:
+                return self.playlist_result(entries, "gaybeegplaylist", "gaybeegplaylist")
+            else: raise ExtractorError("No entries")
+            
+        except ExtractorError as e:                 
+            raise 
+        except Exception as e:
+            lines = traceback.format_exception(*sys.exc_info())
+            self.to_screen(f'{type(e)} \n{"!!".join(lines)}')  
+            raise ExtractorError(str(e)) from e
+
+        finally:
+            try:
+                self.rm_driver(driver)
+            except Exception:
+                pass
+
+class GayBeegIE(GayBeegBaseIE):
+    IE_NAME = "gaybeeg:post:playlist"
+    _VALID_URL = r'https?://(www\.)?gaybeeg\.info/\d\d\d\d/\d\d/\d\d/.*'
+    
+               
+    def _real_extract(self, url):        
+        
+        try:
+                 
+            self.report_extraction(url)
+            
+            entries = self._get_entries(url)            
+                        
+            if not entries:
+                raise ExtractorError("No video entries")
+            else:
+                return{
+                    '_type': "playlist",
+                    'id': "gaybeegpost",
+                    'title': "gaybeegpost",
+                    'entries': entries
+                }      
+            
+        except ExtractorError as e:                 
+            raise 
+        except Exception as e:
+            lines = traceback.format_exception(*sys.exc_info())
+            self.to_screen(f'{type(e)} \n{"!!".join(lines)}')  
+            raise ExtractorError(str(e)) from e
--- yt_dlp/extractor/sketchysex.py
+++ yt_dlp/extractor/sketchysex.py
@@ -1,1 +1,365 @@
-404: Not Found
+# coding: utf-8
+from __future__ import unicode_literals
+
+
+import re
+
+
+from ..utils import (
+    ExtractorError,
+    sanitize_filename,
+    std_headers
+)
+
+
+import threading
+import traceback
+import sys
+
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By
+
+
+import demjson
+
+from .webdriver import SeleniumInfoExtractor
+
+import httpx
+
+class SketchySexBaseIE(SeleniumInfoExtractor):
+    _LOGIN_URL = "https://sketchysex.com/sign-in"
+    _SITE_URL = "https://sketchysex.com"
+    _LOGOUT_URL = "https://sketchysex.com/sign-out"
+    _MULT_URL = "https://sketchysex.com/multiple-sessions"
+    _ABORT_URL = "https://sketchysex.com/multiple-sessions/abort"
+    _AUTH_URL = "https://sketchysex.com/authorize2"
+    _BASE_URL_PL = "https://sketchysex.com/episodes/"
+
+    _NETRC_MACHINE = 'sketchysex'
+
+    _LOCK = threading.Lock()
+    
+    
+    _COOKIES = None 
+
+
+    def _login(self, _driver):
+        
+        _driver.get(self._SITE_URL)
+        _title = _driver.title.upper()
+        #self.to_screen(_title)
+        if "WARNING" in _title:
+            self.to_screen("Adult consent")
+            el_enter = self.wait_until(_driver, 60, ec.presence_of_element_located((By.CLASS_NAME, "enter-btn")))
+            if not el_enter: raise ExtractorError("couldnt find adult consent button")
+            _current_url = _driver.current_url
+            el_enter.click()
+            self.wait_until(_driver, 60, ec.url_changes(_current_url))
+        
+        el_top = self.wait_until(_driver, 60, ec.presence_of_element_located((By.CSS_SELECTOR, "ul.inline-list.top-navbar")))
+        if "MEMBERS" in el_top.get_attribute('innerText').upper():
+            self.report_login()
+            username, password = self._get_login_info()
+
+            
+            
+            if not username or not password:
+                self.raise_login_required(
+                    'A valid %s account is needed to access this media.'
+                    % self._NETRC_MACHINE)
+            
+            _driver.get(self._LOGIN_URL)
+            el_username = self.wait_until(_driver, 60, ec.presence_of_element_located((By.CSS_SELECTOR, "input#username")))
+            el_password = self.wait_until(_driver, 60, ec.presence_of_element_located((By.CSS_SELECTOR, "input#password")))
+            el_login = _driver.find_element(by=By.CSS_SELECTOR, value="input#submit1.submit1")
+            if not el_username or not el_password or not el_login: raise ExtractorError("couldnt find text elements")
+            el_username.send_keys(username)
+            self.wait_until(_driver, 3, ec.title_is("DUMMYFORWAIT"))
+            el_password.send_keys(password)
+            self.wait_until(_driver, 3, ec.title_is("DUMMYFORWAIT"))
+            #_title = _driver.title
+            _current_url = _driver.current_url
+            #self.to_screen(f"{_title}#{driver.current_url}")
+            el_login.click()
+            self.wait_until(_driver, 60, ec.url_changes(_current_url))
+            count = 3
+            while count > 0:
+            
+                if "episodes" in _driver.current_url:
+                    el_top = self.wait_until(_driver, 60, ec.presence_of_element_located((By.CSS_SELECTOR, "ul.inline-list.top-navbar")))
+                    if not "LOG OUT" in el_top.get_attribute('innerText').upper():
+                        raise ExtractorError("Login failed")
+                    else: break
+                if "authorize2" in _driver.current_url:
+                    el_email = self.wait_until(_driver, 60, ec.presence_of_element_located((By.CSS_SELECTOR, "input#email")))
+                    el_lastname = self.wait_until(_driver, 60, ec.presence_of_element_located((By.CSS_SELECTOR, "input#last-name")))
+                    el_enter = _driver.find_element(by=By.CSS_SELECTOR, value="button")
+                    if not el_email or not el_lastname or not el_enter: raise ExtractorError("couldnt find text elements")
+                    el_email.send_keys("a.tgarc@gmail.com")
+                    self.wait_until(_driver, 3, ec.title_is("DUMMYFORWAIT"))
+                    el_lastname.send_keys("Torres")
+                    self.wait_until(_driver, 3, ec.title_is("DUMMYFORWAIT"))                
+                    _current_url = _driver.current_url
+                    el_enter.click()
+                    self.wait_until(_driver, 60, ec.url_changes(_current_url))
+                if "multiple-sessions" in _driver.current_url:                
+                    self.to_screen("Abort existent session")
+                    el_abort = self.wait_until(_driver, 60, ec.presence_of_element_located((By.CSS_SELECTOR,"button.std-button")))
+                    if not el_abort: raise ExtractorError("couldnt find button to abort sessions")
+                    _current_url = _driver.current_url
+                    el_abort.click()
+                    self.wait_until(_driver, 60, ec.url_changes(_current_url))
+                
+                count -= 1
+                
+            if count == 0: raise ExtractorError("couldnt log in")
+        
+        self.to_screen("Login OK")    
+            
+    def _logout(self, _driver):
+        _driver.get(self._LOGOUT_URL)
+
+    def _extract_from_page(self, cl, url):
+  
+        res = cl.get(url)
+        _title = None
+        mobj = re.findall(r'<h1>([^\<]+)<', res.text)        
+        if mobj:
+            _title = mobj[0]
+        
+        if not _title: _title = url.split('/')[-1].replace("-","_").upper() 
+        #self.to_screen(_title)
+        mobj = re.findall(r'<iframe src=\"([^\"]+)\"', res.text)
+        if mobj:
+            embedurl = mobj[0]
+        else: raise ExtractorError("not embed url")
+        
+        try:
+            res2 = cl.get(embedurl)
+            mobj = re.findall(r'globalSettings\s+\=\s+([^;]*);',res2.text)
+        except Exception:
+            mobj = None
+        if not mobj: raise ExtractorError("no token")
+        _data_dict = demjson.decode(mobj[0])
+        tokenid = _data_dict.get('token')
+        if not tokenid: raise ExtractorError("no token")            
+        #self.to_screen(f"tokenid:{tokenid}") 
+        videourl = "https://videostreamingsolutions.net/api:ov-embed/parseToken?token=" + tokenid
+        #self.to_screen(videourl)
+        headers = dict()
+        headers.update({
+            "Referer" : embedurl,
+            "Accept" : "*/*",
+            "X-Requested-With" : "XMLHttpRequest"})
+        
+        try:
+            res3 = cl.get(videourl, headers=headers)
+            info = res3.json()
+        except Exception:
+            info = None
+        if not info: raise ExtractorError("Can't find any JSON info")
+
+        #print(info)
+        videoid = str(info.get('xdo',{}).get('video', {}).get('id', {}))
+        manifestid = str(info.get('xdo',{}).get('video', {}).get('manifest_id', {}))
+        manifesturl = "https://videostreamingsolutions.net/api:ov-embed/manifest/" + manifestid + "/manifest.m3u8"
+        
+        try:
+            res = cl.get(manifesturl)
+            res.raise_for_status()
+            if not res or not res.content: raise ExtractorError("Cant get m3u8 doc")
+            m3u8_doc = (res.content).decode('utf-8', 'replace')        
+            formats_m3u8, subtitles = self._parse_m3u8_formats_and_subtitles(
+                m3u8_doc, manifesturl, ext="mp4", entry_protocol='m3u8_native', m3u8_id="hls")
+
+            if not formats_m3u8:
+                raise ExtractorError("Can't find any M3U8 format")
+
+            self._sort_formats(formats_m3u8)
+    
+                    
+            return ({
+                "id": videoid,
+                "title": _title,
+                "formats": formats_m3u8
+            })
+        except Exception as e:
+            raise ExtractorError(f"Can't get M3U8 details: {repr(e)}")
+            
+        
+        
+       
+  
+    def _extract_list(self, _driver, playlistid, nextpages):
+        
+        entries = []
+
+        i = 0
+
+        while True:
+
+            url_pl = f"{self._BASE_URL_PL}{int(playlistid) + i}"
+
+            #self.to_screen(url_pl)
+            
+            _driver.get(url_pl)
+            el_listmedia = self.wait_until(_driver, 60, ec.presence_of_all_elements_located((By.CLASS_NAME, "content")))
+            if not el_listmedia: raise ExtractorError("no info")
+            for media in el_listmedia:
+                el_tag = media.find_element(by=By.TAG_NAME, value="a")
+                
+                _title = el_tag.get_attribute("innerText").replace(" ","_")
+                _title = sanitize_filename(_title, restricted=True)
+                entries.append(self.url_result(el_tag.get_attribute("href"), ie=SketchySexIE.ie_key(), video_title=_title))      
+
+            
+            
+            if not nextpages: break
+            
+            if "NEXT" in _driver.page_source:
+                i += 1
+            else:
+                break
+
+        if not entries: raise ExtractorError("no videos found")
+
+        return entries
+
+
+class SketchySexIE(SketchySexBaseIE):
+    IE_NAME = 'sketchysex'
+    IE_DESC = 'sketchysex'
+    _VALID_URL = r'https?://(?:www\.)?sketchysex.com/episode/.*'
+
+    def _real_initialize(self):
+        
+        with SketchySexIE._LOCK:
+            if SketchySexIE._COOKIES:
+                return  
+
+            driver = self.get_driver()
+
+            
+            try:
+                
+                self._login(driver)                
+                
+                SketchySexIE._COOKIES = driver.get_cookies()
+             
+            
+            except Exception as e:
+                self.to_screen("error when login")
+                raise
+            finally:
+                self.rm_driver(driver)
+            
+
+    def _real_extract(self, url):
+        
+        
+        data = None
+        try:
+            
+            url_proxy = self._downloader.params.get('proxy', "")            
+            if url_proxy:
+                if not url_proxy.startswith("http://"): url_proxy = f"http://{url_proxy}"
+                proxies = {'http://': url_proxy, 'https://': url_proxy}                
+            else:
+                proxies = None                
+            cl = httpx.Client(trust_env=False, verify=False, proxies=proxies, headers=std_headers, timeout=httpx.Timeout(15, connect=30), follow_redirects=True, limits=httpx.Limits(max_keepalive_connections=None, max_connections=None))
+            for cookie in SketchySexIE._COOKIES:
+                cl.cookies.set(name=cookie['name'], value=cookie['value'], domain=cookie['domain'])
+                
+            data = self._extract_from_page(cl, url)
+                 
+            
+        except Exception as e:
+            lines = traceback.format_exception(*sys.exc_info())
+            self.to_screen(f"{repr(e)} {str(e)} \n{'!!'.join(lines)}")
+            if "ExtractorError" in str(e.__class__): raise
+            else: raise ExtractorError(str(e))
+        finally:
+            cl.close()
+
+        if not data:
+            raise ExtractorError("Not any video format found")
+        elif "error" in data['id']:
+            raise ExtractorError(str(data['cause']))
+        else:
+            return(data)
+
+class SketchySexOnePagePlaylistIE(SketchySexBaseIE):
+    IE_NAME = 'sketchysex:playlist'
+    IE_DESC = 'sketchysex:playlist'
+    _VALID_URL = r"https?://(?:www\.)?sketchysex\.com/episodes/(?P<id>\d+)"
+
+
+    def _real_extract(self, url):
+
+        playlistid = re.search(self._VALID_URL, url).group("id")
+        
+        entries = None
+        
+        try:              
+                        
+            driver = self.get_driver()
+            
+
+            
+            driver.get(self._SITE_URL)
+            
+            _title = driver.title.lower()
+            
+            if "warning" in _title:
+                el_enter = self.wait_until(driver, 60, ec.presence_of_element_located((By.CLASS_NAME, "enter-btn")))
+                if el_enter: el_enter.click()
+            
+            entries = self._extract_list(driver, playlistid, nextpages=False)  
+       
+        except ExtractorError:
+            raise
+        except Exception as e:
+            lines = traceback.format_exception(*sys.exc_info())
+            self.to_screen(f"{repr(e)}\n{'!!'.join(lines)}")
+            raise ExtractorError(repr(e))
+        finally:
+            self.rm_driver(driver)
+            
+        if not entries: raise ExtractorError("no video list") 
+        
+        return self.playlist_result(entries, f"sketchysex:page_{playlistid}", f"sketchysex:page_{playlistid}")
+    
+class SketchySexAllPagesPlaylistIE(SketchySexBaseIE):
+    IE_NAME = 'sketchysex:allpages:playlist'
+    IE_DESC = 'sketchysex:allpages:playlist'
+    _VALID_URL = r"https?://(?:www\.)?sketchysex\.com/episodes/?$"
+   
+ 
+    def _real_extract(self, url):
+        
+        entries = None
+        
+        try:              
+
+
+            driver = self.get_driver()
+            
+            driver.get(self._SITE_URL)
+            _title = driver.title.lower()            
+            if "warning" in _title:
+                el_enter = self.wait_until(driver, 60, ec.presence_of_element_located((By.CLASS_NAME, "enter-btn")))
+                if el_enter: el_enter.click()
+                
+            entries = self._extract_list(driver, 1, nextpages=True)  
+        
+        except Exception as e:
+            lines = traceback.format_exception(*sys.exc_info())
+            self.to_screen(f"{repr(e)} {str(e)} \n{'!!'.join(lines)}")
+            if "ExtractorError" in str(e.__class__): raise
+            else: raise ExtractorError(str(e))
+        finally:
+            self.rm_driver(driver)
+            
+        if not entries: raise ExtractorError("no video list") 
+        
+        return self.playlist_result(entries, f"sketchysex:AllPages", f"sketchysex:AllPages")
