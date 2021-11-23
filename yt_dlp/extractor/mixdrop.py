c96a8dde79629fb8467075718aa91658a368569b
atgarcia78 <a.tgarc@gmail.com>
2021-11-23 04:44:22 +0100

    upt

---
+++ yt_dlp/extractor/mixdrop.py
@@ -0,0 +1,120 @@
+# coding: utf-8
+from __future__ import unicode_literals
+
+from ..utils import (
+    ExtractorError,
+    sanitize_filename,
+    
+)
+
+
+import traceback
+import sys
+
+
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By
+
+
+from threading import Lock
+
+from .webdriver import SeleniumInfoExtractor
+
+from ratelimit import (
+    sleep_and_retry,
+    limits
+)
+
+
+class MixDropIE(SeleniumInfoExtractor):
+    
+    _SITE_URL = "https://mixdrop.co"
+    
+    IE_NAME = 'mixdrop'
+    _VALID_URL = r'https?://(?:www\.)?mixdrop.co/(?:e|f)/(?P<id>[^\/$]+)(?:\/|$)'
+
+    _LOCK = Lock()
+   
+
+    @sleep_and_retry
+    @limits(calls=1, period=10)
+    def _get_video_info(self, url):
+        
+
+        self.to_screen(f"[get_video_info] {url}")
+        return self.get_info_for_format(url)       
+
+    @sleep_and_retry
+    @limits(calls=1, period=10)
+    def _send_request(self, driver, url):
+
+
+        self.to_screen(f"[send_request] {url}")        
+        driver.get(url)
+         
+
+    def _real_extract(self, url):
+        
+        self.report_extraction(url)
+
+        driver = self.get_driver() 
+            
+        try:            
+            
+            _url = url.replace('/e/', '/f/')
+            self._send_request(driver, _url)
+            _title =  driver.title.replace("MixDrop", "").replace("Watch", "").replace("-", "").strip()
+            el_fr = self.wait_until(driver, 60, ec.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR, "iframe")))
+            if not el_fr: raise ExtractorError("no videoframe")
+            el_button = self.wait_until(driver, 60, ec.presence_of_element_located((By.CSS_SELECTOR, "button.vjs-big-play-button")))
+            el_video = self.wait_until(driver, 60, ec.presence_of_element_located((By.ID, "videojs_html5_api")))
+            if not el_video: raise ExtractorError("no info")
+            if el_button:
+                try:
+                    el_button.click()
+                    self.wait_until(driver, 3, ec.title_is("DUMMYFORWAIT"))
+                except Exception:
+                    pass
+                               
+            video_url = el_video.get_attribute("src")
+            if not video_url: raise ExtractorError("no video url") 
+            _videoid = self._match_id(url)
+            _videoinfo = self._get_video_info(video_url)
+            if _videoinfo.get('error'): raise ExtractorError("error video info")
+            
+            _format = {
+                    'format_id': 'http-mp4',
+                    'url': _videoinfo['url'],
+                    'filesize': _videoinfo['filesize'],
+                    'ext': 'mp4'
+            }
+            
+            _entry_video = {
+                'id' : _videoid,
+                'title' : sanitize_filename(_title, restricted=True),
+                'formats' : [_format],
+                'ext': 'mp4'
+            }
+            
+            if not _entry_video: raise ExtractorError("no video info")
+            else:  return _entry_video   
+            
+        except ExtractorError:
+            raise
+        except Exception as e:
+            lines = traceback.format_exception(*sys.exc_info())
+            self.to_screen(f"{repr(e)}\n{'!!'.join(lines)}")
+            raise ExtractorError(repr(e))
+        finally:
+            try:
+                self.rm_driver(driver)
+            except Exception:
+                pass
+        
+        
+     
+               
+        
+
+      
+
