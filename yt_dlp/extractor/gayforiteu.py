1d39aff7865ce7ed23291cfc152b3860fefea3eb
atgarcia78 <a.tgarc@gmail.com>
2021-12-01 20:57:11 +0100

    upt

--- yt_dlp/extractor/gayforiteu.py
+++ yt_dlp/extractor/gayforiteu.py
@@ -3,28 +3,42 @@ from __future__ import unicode_literals
 
 import re
 
-from .common import InfoExtractor
+from .webdriver import SeleniumInfoExtractor
 from ..utils import (
-    ExtractorError,
-    HEADRequest,
-    int_or_none,
-    sanitize_filename
+    ExtractorError, 
+    sanitize_filename,
+    
 )
 
+from urllib.parse import unquote
+from ratelimit import limits, sleep_and_retry
+from backoff import on_exception, constant
 
-class GayForITEUIE(InfoExtractor):
+
+class GayForITEUIE(SeleniumInfoExtractor):
     
     _VALID_URL = r'https?://(?:www\.)gayforit\.eu/(?:playvideo.php\?vkey\=[^&]+&vid\=(?P<vid>[\w-]+)|video/(?P<id>[\w-]+))'
 
+    
+    @on_exception(constant, Exception, max_tries=5, interval=1)
+    @sleep_and_retry
+    @limits(calls=1, period=1)
+    def _send_request(self, url, videoid):
+        
+        webpage = self._download_webpage(url, videoid, "Downloading video webpage", fatal=True)
+        if not webpage: raise ExtractorError("no video page info")
+        else: return webpage
+    
+    
     def _real_extract(self, url):
         
-        videoid = self._search_regex(self._VALID_URL, url, 'videoid',default=None, fatal=False, group='id' )
-        if not videoid:
-            videoid = self._search_regex(self._VALID_URL, url, 'videoid',default=None, fatal=False, group='vid')
-            
-            
+        mobj = re.search(self._VALID_URL, url)
+        if mobj:
+            videoid = mobj.group('id') or mobj.group('vid')
 
-        webpage = self._download_webpage(url, videoid, "Downloading video webpage")
+        webpage = self._send_request(url, videoid) 
+        
+        if not webpage: raise ExtractorError("no video page info")
 
         title = self._html_search_regex((r'<title>GayForIt\.eu - Free Gay Porn Videos - (.+?)</title>'), webpage, 'title')
         
@@ -35,20 +49,20 @@ class GayForITEUIE(InfoExtractor):
 
         if not video_url:
             raise ExtractorError("no video url")
+        else: video_url = unquote(video_url)
         
         if not videoid:
             videoid = self._search_regex(r'content/(\d+)/mp4', video_url, 'videoid', default="no_id")
   
-
-
-        res = self._request_webpage(HEADRequest(video_url), videoid, headers={'Referer' : url, 'Range' : 'bytes=0-'})
-
-        filesize =  int_or_none(res.headers.get('Content-Length'))
+        self.to_screen(f"[video_url] {video_url}")
+        _info_video = self.get_info_for_format(video_url, headers={"Referer" : "https://gayforit.eu/"}, verify=False)
         
+        if not _info_video: raise ExtractorError("no video info")
+
         format_video = {
             'format_id' : "http-mp4",
-            'url' : video_url,
-            'filesize' : filesize,
+            'url' : _info_video['url'],
+            'filesize' : _info_video['filesize'],
             'ext' : 'mp4'
          }
 
