7c03029d1db0908d6a334cc373fc7835e9ff8d30
atgarcia78 <a.tgarc@gmail.com>
2022-03-25 19:13:48 +0100

    upt

--- yt_dlp/extractor/gvdblog.py
+++ yt_dlp/extractor/gvdblog.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 import re
 
 from datetime import datetime
+import json
 
 from backoff import on_exception
 
@@ -96,4 +97,69 @@ class GVDBlogPostIE(SeleniumInfoExtractor):
             lines = traceback.format_exception(*sys.exc_info())
             self.to_screen(f'{type(e)} \n{"!!".join(lines)}')  
             raise ExtractorError(str(e))
+        
+class GVDBlogPlaylistIE(SeleniumInfoExtractor):
+    IE_NAME = "gvdblog:playlist"
+    _VALID_URL = r'https?://(?:www\.)?gvdblog.com/search\?(?P<query>.+)'
+    
+    @on_exception(constant, Exception, max_tries=5, interval=0.1)
+    @limiter_0_1.ratelimit("gvdblog", delay=True)
+    def _send_request(self, url):        
+        
+        self.logger_info(f"[send_request] {url}") 
+        res = self._CLIENT.get(url)
+        return res
+    
+    def _real_initialize(self):
+        super()._real_initialize()
+               
+    def _real_extract(self, url):
+        
+        def getter(x):
+
+            if not x:
+                return []
+            if _jsonstr:=x.group("data"):
+                return json.loads(_jsonstr).get('feed', {}).get('entry', [])
+            
+        def get_videourl(x):
+            if len(x) > 1:
+                for el in x:
+                    if not 'dood.' in el and not '.gs/' and not 'imdb.com' in el: return el
+            else: 
+                if not '.gs/' in x[0]: return x[0]
+            
+        
+        
+        query = re.search(self._VALID_URL, url).group('query')
+        
+        params = { el.split('=')[0]: el.split('=')[1] for el in query.split('&')}
+        
+        urlquery = f"https://www.gvdblog.com/feeds/posts/full?alt=json-in-script&max-results=9999"
+        
+        if _category:=params.get('label'):
+            urlquery += f"&category={_category}"
+            
+        res = self._send_request(urlquery)
+        if not res: raise ExtractorError("no search results")
+        video_entries = try_get(re.search(r"gdata.io.handleScriptLoaded\((?P<data>.*)\);", res.text), getter)
+        if not video_entries: raise ExtractorError("no video entries")
+        _entries = []
+        for _entry in video_entries:
+            postdate = datetime.strptime(_entry['published']['$t'].split('T')[0], '%Y-%m-%d')
+            videourlpost = _entry['link'][-1]['href']
+            videourl = try_get(re.findall(r'href="([^" ]+)" target=', _entry['content']['$t']), get_videourl) or try_get(re.findall(r'iframe[^>]*src=[\"\']([^\"\']+)[\"\']', _entry['content']['$t']), lambda x: x[0])
+            _entries.append({
+                '_type': 'url_transparent',
+                'original_url': videourlpost,
+                'webpage_url': url,
+                'release_date': postdate.strftime('%Y%m%d'),
+                'release_timestamp': int(postdate.timestamp()),
+                'url': videourl
+            })
+        
+        if not _entries: raise ExtractorError("no video list")
+        return self.playlist_result(_entries, f"gvdblog_playlist", f"gvdblog_playlist")
+             
+        
         
