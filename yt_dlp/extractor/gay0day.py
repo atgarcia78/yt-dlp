bfb04761e3c2bfa41166d75310967048fe816636
atgarcia78 <a.tgarc@gmail.com>
2022-04-03 17:59:41 +0200

    upt

--- yt_dlp/extractor/gay0day.py
+++ yt_dlp/extractor/gay0day.py
@@ -2,106 +2,100 @@ from __future__ import unicode_literals
 
 
 import re
-from .common import InfoExtractor
+from .commonwebdriver import (
+    SeleniumInfoExtractor,
+    limiter_1
+)
+
 from ..utils import (
     ExtractorError,   
     sanitize_filename,
-    int_or_none,
-    js_to_json
-
+    js_to_json,
+    try_get
 )
 
-import httpx
 import traceback
 import sys
 import json
-import time
 
+from backoff import constant, on_exception
 
-
-class Gay0DayIE(InfoExtractor):
+class Gay0DayIE(SeleniumInfoExtractor):
     
     IE_NAME = 'gay0day'
-    _VALID_URL = r'https?://(www\.)?gay0day\.com/(?:videos|embed)/(?P<id>\d+)/?(?P<title>[^\$]*)$'
+    _VALID_URL = r'https?://(www\.)?gay0day\.com/(.+/)?(?:videos|embed)/(?P<id>\d+)/?(?P<title>[^$/]*)'
     
-     
-    def get_filesize(self, url, client):
+    @on_exception(constant, Exception, max_tries=5, interval=1)
+    @limiter_1.ratelimit("gay0day", delay=True)
+    def _send_request(self, url, _type="GET", data=None, headers=None):        
         
-        count = 0
+        res = None
         try:
-            
-            _res = None
-            while (count<3):
-                
-                try:
-                    
-                    res = client.head(url)
-                    if res.status_code > 400:
-                        time.sleep(10)
-                        count += 1
-                    else: 
-                        _res = int_or_none(res.headers.get('content-length')) 
-                        break
-            
-                except Exception as e:
-                    count += 1
+            self.logger_info(f"[_send_request] {url}") 
+            res = self.send_request(url, _type=_type, data=data, headers=headers)
+            res.raise_for_status()
+            return res
         except Exception as e:
-            pass
-
+            if res: 
+                msg_error = f'{res} - {res.request} \n{res.request.headers}'
+            else: msg_error = ""
+            self.logger_info(f"[_send_request][{url}] error {repr(e)} - {msg_error}")
+            raise
         
-        return _res
+    @on_exception(constant, Exception, max_tries=5, interval=1)
+    @limiter_1.ratelimit("gay0day", delay=True)   
+    def _get_info_for_format(self, *args, **kwargs):
+        return super().get_info_for_format(*args, **kwargs)
     
-   
+    
+    def _real_initialize(self):
+        super()._real_initialize()   
 
-    def _real_extract(self, url):
-        
+    def _real_extract(self, url):        
                        
         self.report_extraction(url)
         
         try:
             
-            client = httpx.Client(headers=self.get_param('http_headers'),timeout=60,verify=(not self._downloader.params.get('nocheckcertificate')))
+            webpage = try_get(self._send_request(url), lambda x: x.text)
+            if not webpage: raise ExtractorError("couldnt download webpage")
+            _info_flashvars = try_get(re.findall(r'(?ms)<script.*?>.*?var\s+flashvars\s*=\s*(\{.*?\});.*?</script>', webpage), lambda x: json.loads(js_to_json(x[0])))
+            _entry_video = {}            
+            if _info_flashvars: 
             
-            res = client.get(url)
-            if res.status_code > 400: raise ExtractorError(f'{url}:{res}')
-            else: webpage = res.text
-            flashvars = re.findall(r'(?ms)<script.*?>.*?var\s+flashvars\s*=\s*(\{.*?\});.*?</script>', webpage)
-            _entry_video = {}
-            if flashvars:
-                info_video = json.loads(js_to_json(flashvars[0]))
-                if info_video: 
-            
-                    _url = info_video.get("video_url")
-                    _res = info_video.get("postfix").replace("_","").replace(".mp4","")            
-                    _format = {
-                        'format_id': _res,
-                        'resolution': _res,
-                        'url': _url,
-                        'ext': 'mp4',
-                        'filesize': self.get_filesize(_url, client)
-                    }
-                    _videoid = info_video.get("video_id")
-                    if not _videoid: _videoid = self._match_id(url)
-                    mobj = re.findall(r"<title>([^<]+)<", webpage)
-                    _title = mobj[0] if mobj else "video_gaydgay"
-                    _title = _title.replace("Video:","").replace("at Gay0Day","").strip()
-                    _entry_video = {
-                        'id' : _videoid,
-                        'title' : sanitize_filename(_title, restricted=True),
-                        'formats' : [_format],
-                        'ext': 'mp4'}
+                _url = _info_flashvars.get("video_url")
+                _res = _info_flashvars.get("postfix").replace("_","").replace(".mp4","")
+                _headers = {'Referer': url}
+                _info_video = self._get_info_for_format(_url, headers=_headers)
+                if not _info_video: raise ExtractorError('couldnt get info video')           
+                _format = {
+                    'format_id': _res,
+                    'resolution': _res,
+                    'url': _info_video.get('url'),
+                    'ext': 'mp4',
+                    'filesize': _info_video.get('filesize'),
+                    'http_headers': _headers
+                }
+                _videoid = _info_video.get("video_id")
+                if not _videoid: _videoid = self._match_id(url)
+                _title = try_get(re.findall(r"<title>([^<]+)<", webpage), lambda x: x[0].replace("Video:","").replace("at Gay0Day","").replace("en Gay0Day","").strip()) 
+                if not _title:
+                    _title = try_get(re.search(self._VALID_URL, url), lambda x: x.group('title').replace('-', '_'))
+                _entry_video = {
+                    'id' : _videoid,
+                    'title' : sanitize_filename(_title, restricted=True),
+                    'formats' : [_format],
+                    'ext': 'mp4'}
+                
+                return _entry_video
         
+        except ExtractorError:
+            raise
         except Exception as e:
             lines = traceback.format_exception(*sys.exc_info())
             self.to_screen(f"{repr(e)} {str(e)} \n{'!!'.join(lines)}")
-            if "ExtractorError" in str(e.__class__): raise
-            else: raise ExtractorError(str(e))
-        finally:
-            client.close()
-        
-        if not _entry_video: raise ExtractorError("no video info")
-        else:
-            return _entry_video    
+            raise ExtractorError(str(e))
+
             
                         
 
