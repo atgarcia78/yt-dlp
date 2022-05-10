22a8e33bc25587f7a0d23f4fa7882a9ddf286b49
atgarcia78 <a.tgarc@gmail.com>
2022-05-10 10:55:32 +0200

    upt

--- yt_dlp/extractor/trafficdepot.py
+++ yt_dlp/extractor/trafficdepot.py
@@ -7,39 +7,21 @@ from .commonwebdriver import (
 )
 
 from ..utils import (
-    ExtractorError,
-    sanitize_filename,
+    ExtractorError
+
 )
-
-from selenium.webdriver.support import expected_conditions as ec
-from selenium.webdriver.common.by import By
-
-
-
 
 import traceback
 import sys
 
 
-
-
 from backoff import constant, on_exception
 
-class get_videourl():
-    def __call__(self, driver):
-        elcont = driver.find_element(By.CSS_SELECTOR, "html")
-        elcont.click()
-        elcont2 = driver.find_element(By.CSS_SELECTOR, "div.loading-container.faplbu")
-        elcont2.click()
-        elvid = driver.find_element(By.TAG_NAME, "video")
-        videourl = elvid.get_attribute('src')
-        if not videourl: return False
-        else: return videourl
         
 
-class GayMovieSupportIE(SeleniumInfoExtractor):
-    IE_NAME = 'gaymoviesupport'
-    _VALID_URL = r'https?://gaymoviesupport\.[^/]+/v/(?P<id>.*)'
+class TrafficDePotIE(SeleniumInfoExtractor):
+    IE_NAME = 'trafficdepot'
+    _VALID_URL = r'https?://trafficdepot\.xyz/v/(?P<id>.*)'
 
     def _get_video_info(self, url):        
         self.logger_info(f"[get_video_info] {url}")
@@ -52,7 +34,7 @@ class GayMovieSupportIE(SeleniumInfoExtractor):
     
     
     @on_exception(constant, Exception, max_tries=5, interval=1)    
-    @limiter_1.ratelimit("gaymoviesupport", delay=True)
+    @limiter_1.ratelimit("trafficdepot", delay=True)
     def request_to_host(self, _type, *args, **kwargs):
     
         if _type == "video_info":
@@ -60,38 +42,32 @@ class GayMovieSupportIE(SeleniumInfoExtractor):
         elif _type == "url_request":
             self._send_request(*args)
         elif _type == "post":
-            res = self._CLIENT.post(*args, **kwargs)
+            res = TrafficDePotIE._CLIENT.post(*args, **kwargs)
             res.raise_for_status()
             return(res.json())    
     
-    def _real_initialize(self):
-        super()._real_initialize()
-
-    def _real_extract(self, url):
-
+    def _get_video_entry(self, url):
         
-        self.report_extraction(url)
+        _headers = {
+            'X-Requested-With': 'XMLHttpRequest', 
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8', 
+            'Referer': url,
+            'Origin': 'https://trafficdepot.xyz'
+        }
+        _data =  {"r": "https://porndune.com/", "d": "trafficdepot.xyz"}
         
-        #driver = self.get_driver(usequeue=True)
- 
-        try:
+        videoid = self._match_id(url)
             
-            videoid = self._match_id(url)
-            _headers = {
-                'X-Requested-With': 'XMLHttpRequest', 
-                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8', 
-                'Referer': url,
-                'Origin': 'https://gaymoviesupport.cf'
-            }
-            _data =  {"r": "", "d": "gaymoviesupport.cf"}
-                
-            videojson = self.request_to_host("post", f"https://gaymoviesupport.cf/api/source/{videoid}", data=_data, headers=_headers)
+        try:
+        
+            videojson = self.request_to_host("post", f"https://trafficdepot.xyz/api/source/{videoid}", data=_data, headers=_headers)
 
-            #video_url = self.wait_until(driver, 30, get_videourl())
 
+            _entry_video = {}
+            
             if videojson.get('data'):
                 self.to_screen(videojson['data'])
-                #_info = self.request_to_host("video_info", video_url)
+                
                 _formats = []
                 for _format in videojson['data']:
                     _videourl = _format.get('file')
@@ -116,14 +92,43 @@ class GayMovieSupportIE(SeleniumInfoExtractor):
                     
                     _formats.append(_format_video)
                                     
-                _entry_video = {
-                    'id' : videoid,                    
-                    'formats' : _formats,
-                    'ext': "mp4"
-                }
+                if _formats: 
+                    self._sort_formats(_formats)
                 
-                return _entry_video
-            else: raise ExtractorError("couldnt get video info")
+                    _entry_video = {
+                        'id' : videoid,                    
+                        'formats' : _formats,
+                        'ext': "mp4"
+                    }
+                
+                    return _entry_video
+                
+            if not _entry_video: raise ExtractorError("no entry video")
+                
+                    
+        except ExtractorError:
+            raise
+        except Exception as e:
+            raise ExtractorError(repr(e))
+        
+        
+        
+        
+    
+    def _real_initialize(self):
+        super()._real_initialize()
+
+    def _real_extract(self, url):
+
+        
+        self.report_extraction(url)
+        
+     
+        try:
+            
+            
+            return self._get_video_entry(url)
+        
             
         except ExtractorError as e:
             raise
@@ -131,8 +136,4 @@ class GayMovieSupportIE(SeleniumInfoExtractor):
             lines = traceback.format_exception(*sys.exc_info())
             self.to_screen(f"{repr(e)}\n{'!!'.join(lines)}")
             raise ExtractorError(repr(e))
-        # finally:
-        #     try:
-        #         self.put_in_queue(driver)
-        #     except Exception:
-        #         pass
+
