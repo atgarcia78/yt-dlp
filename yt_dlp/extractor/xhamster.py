b8a19587b32a7d935e0e472c8a3407c3afffbc4c
atgarcia78 <a.tgarc@gmail.com>
2022-05-27 02:40:24 +0200

    upt

--- yt_dlp/extractor/xhamster.py
+++ yt_dlp/extractor/xhamster.py
@@ -452,3 +452,46 @@ class XHamsterUserIE(InfoExtractor):
     def _real_extract(self, url):
         user_id = self._match_id(url)
         return self.playlist_result(self._entries(user_id), user_id)
+
+
+
+
+
+class XHamsterPornStarIE(InfoExtractor):
+    _VALID_URL = r'https?://(?:.+?\.)?%s/pornstars/(?P<id>[^/?#&]+)' % XHamsterIE._DOMAINS
+    _TESTS = [{
+        # Paginated user profile
+        'url': 'https://es.xhamster.com/pornstars/chris_torres',
+        'info_dict': {
+            'id': 'chris_torres',
+        },
+        'playlist_mincount': 7,
+    }]
+    
+
+    def _entries(self, user_id):
+        next_page_url = 'https://xhamster.com/pornstars/%s/1' % user_id
+        for pagenum in itertools.count(1):
+            page = self._download_webpage(
+                next_page_url, user_id, 'Downloading page %s' % pagenum)
+            for video_tag in re.findall(
+                    r'(<a[^>]+class=["\'].*?\bvideo-thumb__image-container[^>]+>)',
+                    page):
+                video = extract_attributes(video_tag)
+                video_url = url_or_none(video.get('href'))
+                if not video_url or not XHamsterIE.suitable(video_url):
+                    continue
+                video_id = XHamsterIE._match_id(video_url)
+                yield self.url_result(
+                    video_url, ie=XHamsterIE.ie_key(), video_id=video_id)
+            mobj = re.search(r'<a[^>]+data-page=["\']next[^>]+>', page)
+            if not mobj:
+                break
+            next_page = extract_attributes(mobj.group(0))
+            next_page_url = url_or_none(next_page.get('href'))
+            if not next_page_url:
+                break
+
+    def _real_extract(self, url):
+        user_id = self._match_id(url)
+        return self.playlist_result(self._entries(user_id), user_id)
