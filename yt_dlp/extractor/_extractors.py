d75108bebbe5611ff2634c97d02228efefcaa007
atgarcia78 <a.tgarc@gmail.com>
2022-06-21 15:52:04 +0200

    upt

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -1,5 +1,4 @@
-import contextlib
-import os
+# flake8: noqa: F401
 
 from .youtube import (  # Youtube is moved to the top to improve performance
     YoutubeIE,
@@ -798,24 +797,6 @@ from .itprotv import (
     ITProTVIE,
     ITProTVCourseIE
 )
-_LAZY_LOADER = False
-if not os.environ.get('YTDLP_NO_LAZY_EXTRACTORS'):
-    with contextlib.suppress(ImportError):
-        from .lazy_extractors import *  # noqa: F403
-        from .lazy_extractors import _ALL_CLASSES
-        _LAZY_LOADER = True
-
-if not _LAZY_LOADER:
-    from ._extractors import *  # noqa: F403
-    _ALL_CLASSES = [  # noqa: F811
-        klass
-        for name, klass in globals().items()
-        if name.endswith('IE') and name != 'GenericIE'
-    ]
-    _ALL_CLASSES.append(GenericIE)  # noqa: F405
-
-_PLUGIN_CLASSES = load_plugins('extractor', 'IE', globals())
-_ALL_CLASSES = list(_PLUGIN_CLASSES.values()) + _ALL_CLASSES
 from .itv import (
     ITVIE,
     ITVBTCCIE,
