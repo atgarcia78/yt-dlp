d187e27af4f7e0054d5e70af700fe864bdf5452e
atgarcia78 <a.tgarc@gmail.com>
2022-08-03 11:55:53 +0200

    upt

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -2642,3 +2642,7 @@ from .xhamster import XHamsterPornStarIE
 from .gaymovies import GayMoviesIE
 from .filemoon import FilemoonIE
 from .pornhits import PornhitsIE
+from .upstream import (
+    UpstreamIE,
+    VideobinIE
+)
