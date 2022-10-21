7f3e919e4ff998cca493795a083bea8c3fe073d5
atgarcia78 <a.tgarc@gmail.com>
2022-10-21 19:54:33 +0200

    upt

--- yt_dlp/mylogger.py
+++ yt_dlp/mylogger.py
@@ -23,7 +23,8 @@ class MyLogger(logging.LoggerAdapter):
                     'The information of all playlist entries will be held in memory', 'Looking for video embeds',
                     'Identified a HTML5 media', 'Identified a KWS Player', ' unable to extract', 'Looking for embeds',
                     'Looking for Brightcove embeds', 'Identified a html5 embed', 'mainjs from cache', 'mainjs to cache',
-                    'jscode from cache', 'jscode to cache']
+                    'jscode from cache', 'jscode to cache',
+                    'Loaded ']
     
     _skip_phr = ['Downloading', 'Extracting information', 'Checking', 'Logging']
     
