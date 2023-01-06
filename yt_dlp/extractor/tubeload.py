98e85ace77efca01c729a9ae582b4f767744113a
atgarcia78 <a.tgarc@gmail.com>
2023-01-06 22:33:47 +0100

    upt

--- yt_dlp/extractor/tubeload.py
+++ yt_dlp/extractor/tubeload.py
@@ -17,6 +17,7 @@ from .commonwebdriver import (
     limiter_0_1,
     limiter_non,
     my_dec_on_exception,
+    Union
 )
 from ..utils import (
     ExtractorError,
@@ -33,6 +34,8 @@ class BaseloadIE(SeleniumInfoExtractor):
 
     _LOCK = Lock()
     _IP_ORIG = None
+    _MAINJS = ""
+    _SITE_URL = ""
 
     @on_exception_vinfo
     @dec_on_exception2
@@ -92,7 +95,7 @@ class BaseloadIE(SeleniumInfoExtractor):
         args = try_get(re.findall(r'var .+eval\(.+decodeURIComponent\(escape\(r\)\)\}\(([^\)]+)\)', webpage), lambda x: getter(x))       
         return args
         
-    
+
     def get_mainjs(self, url):
         _headers_mainjs = {    
             'Referer': url,
@@ -105,29 +108,38 @@ class BaseloadIE(SeleniumInfoExtractor):
         
         return(try_get(self._send_request(self._MAINJS, headers=_headers_mainjs), lambda x: x.text))
     
-    def _getres0(self, _url):
+    def _getres0(self, _url)->str:
         if (mainjs := self.get_mainjs(_url)) and (argsjs := self._get_args(mainjs)):            
             cmd0 = "node /Users/antoniotorres/Projects/common/logs/tubeload_deofus.js " + " ".join([str(el) for el in argsjs])
             res0 = subprocess.run(cmd0.split(' '), capture_output=True, encoding="utf-8").stdout.strip('\n')
-            if res0: self.cache.store(self.IE_NAME, f'{self._key}res0', res0)
-            return res0
+            if res0: 
+                self.cache.store(self.IE_NAME, f'{self._key}res0', res0)
+                return res0
+            else:
+                raise ExtractorError("couldnt get res0")
 
-    def _getinfofromwebpage(self, _url, webpage, max_limit, pre):
+        else:
+            raise ExtractorError("couldnt get res0")
+
+    def _getinfofromwebpage(self, _url, webpage, max_limit, pre)->tuple[str,str]:
         _args = None
         title = None
         if not webpage:
             webpage = try_get(self._send_request(_url, max_limit=max_limit), lambda x: html.unescape(x) if isinstance(x, str) else html.unescape(x.text))
-            if not webpage: 
+            if not webpage or '<title>404' in webpage:
                 raise ExtractorError("error 404 no webpage")
-            self.logger.debug(f'{pre} size webpage dl: {len(webpage)}')
-            if '<title>404' in webpage:
-                raise ExtractorError("error 404 no webpage")
-        title = re.sub(r'(?i)((at )?%s$)' % get_domain(self._SITE_URL), '', self._html_extract_title(webpage).replace('.mp4','')).strip('[_,-, ]')
-        _args = self._get_args(webpage)
-        if not _args: 
-            raise ExtractorError("error extracting video args")
-        cmd1 = "node /Users/antoniotorres/Projects/common/logs/tubeload_deofus.js " + " ".join([str(el) for el in _args])
-        return (subprocess.run(cmd1.split(' '), capture_output=True, encoding="utf-8").stdout.strip('\n'), title)
+            self.logger.debug(f'{pre} size webpage dl: {len(webpage)}')            
+                
+        _title = try_get(self._html_extract_title(webpage), lambda x: x.replace('.mp4','').strip('[_,-, ]') if x else None)
+        if not _title:
+            raise ExtractorError("error no title")
+        else:
+            title = re.sub(r'(?i)((at )?%s$)' % get_domain(_url), '', _title)
+            _args = self._get_args(webpage)
+            if not _args: 
+                raise ExtractorError("error extracting video args")
+            cmd1 = "node /Users/antoniotorres/Projects/common/logs/tubeload_deofus.js " + " ".join([str(el) for el in _args])
+            return (subprocess.run(cmd1.split(' '), capture_output=True, encoding="utf-8").stdout.strip('\n'), title)
     
     def _get_entry(self, url, **kwargs):     
 
@@ -141,18 +153,22 @@ class BaseloadIE(SeleniumInfoExtractor):
         videoid = self._match_id(url)
         _url =  f"{self._SITE_URL}/e/{videoid}"
 
-        try:
+        res0 = None
+        res1 = None
+        title = None
 
-            res0 = self.cache.load(self.IE_NAME, f'{self._key}res0')
+        try:
+            res0 = self.cache.load(self.IE_NAME, f'{self._key}res0')            
             if not res0:
                 with ThreadPoolExecutor(thread_name_prefix="tload") as exe:
                     futures = {exe.submit(self._getinfofromwebpage, _url, webpage, max_limit, pre): 'infowebpage', exe.submit(self._getres0, _url): 'res0'}
         
-                for fut in futures:
-                    if 'infowebpage' in futures[fut]:
-                        res1, title = fut.result()
-                    else:
-                        res0 = fut.result()
+                for fut in futures:                    
+                    if (res:=fut.result()):
+                        if isinstance(res, tuple):
+                            res1, title = res
+                        else:
+                            res0 = res
             
             else:
             
@@ -160,39 +176,37 @@ class BaseloadIE(SeleniumInfoExtractor):
 
             if not res0 or not res1:
                 raise ExtractorError(f"error in res0[{not res0}] or res1[{not res1}]")
+            else:
+                video_url = subprocess.run(['node', '/Users/antoniotorres/Projects/common/logs/tubeload_getvurl.js', res0, res1], capture_output=True, encoding="utf-8").stdout.strip('\n')
 
-            video_url = subprocess.run(['node', '/Users/antoniotorres/Projects/common/logs/tubeload_getvurl.js', res0, res1], capture_output=True, encoding="utf-8").stdout.strip('\n')
+                _format = {
+                    'format_id': 'http-mp4',
+                    'url': unquote(video_url),               
+                    'http_headers': {'Referer': f'{self._SITE_URL}/', 'Origin': self._SITE_URL},
+                    'ext': 'mp4'
+                }
 
-            _format = {
-                'format_id': 'http-mp4',
-                'url': unquote(video_url),               
-                'http_headers': {'Referer': f'{self._SITE_URL}/', 'Origin': self._SITE_URL},
-                'ext': 'mp4'
-            }
+                if check:
+                    _host = get_domain(video_url)
+                    
+                    
+                    with self.get_ytdl_sem(_host):
+                        _videoinfo = self._get_video_info(video_url, msg=pre)
+                    if not _videoinfo: raise ExtractorError(f"error 404: no video info")
+                    else:
+                        _format.update({'url': _videoinfo['url'], 'filesize': _videoinfo['filesize'], 'accept_ranges': _videoinfo['accept_ranges']})
 
-            if check:
-                _host = get_domain(video_url)
-                with self.get_param('lock'):
-                    if not (_sem:=traverse_obj(self.get_param('sem'), _host)): 
-                        _sem = Lock()
-                        self.get_param('sem').update({_host: _sem})                    
-                with _sem:
-                    _videoinfo = self._get_video_info(video_url, msg=pre)
-                if not _videoinfo: raise ExtractorError(f"error 404: no video info")
-                else:
-                    _format.update({'url': _videoinfo['url'], 'filesize': _videoinfo['filesize'], 'accept_ranges': _videoinfo['accept_ranges']})
-
-            _entry_video = {
-                'id' : videoid,
-                'title' : sanitize_filename(title, restricted=True),
-                'formats' : [_format],
-                'extractor_key' : self.ie_key(),
-                'extractor': self.IE_NAME,
-                'ext': 'mp4',
-                'webpage_url': url
-            } 
-            
-            return _entry_video
+                _entry_video = {
+                    'id' : videoid,
+                    'title' : sanitize_filename(title, restricted=True),
+                    'formats' : [_format],
+                    'extractor_key' : self.ie_key(),
+                    'extractor': self.IE_NAME,
+                    'ext': 'mp4',
+                    'webpage_url': url
+                } 
+                
+                return _entry_video
             
         except Exception as e:
             self.logger.debug(f"{pre} error {repr(e)} - {str(e)}")
@@ -231,7 +245,7 @@ class BaseloadIE(SeleniumInfoExtractor):
 
 class TubeloadIE(BaseloadIE):
     
-    IE_NAME = 'tubeload'    
+    IE_NAME = 'tubeload'# type: ignore
     _SITE_URL = "https://tubeload.co"
     _VALID_URL = r'https?://(?:www\.)?tubeload.co/(?:e|f)/(?P<id>[^\/$]+)(?:\/|$)'
     _EMBED_REGEX = [r'<iframe[^>]+?src=([\"\'])(?P<url>https?://(www\.)?tubeload\.co/e/.+?)\1']
@@ -242,7 +256,7 @@ class TubeloadIE(BaseloadIE):
 class RedloadIE(BaseloadIE):
     
     _SITE_URL = "https://redload.co"    
-    IE_NAME = 'redload'
+    IE_NAME = 'redload'# type: ignore
     _VALID_URL = r'https?://(?:www\.)?redload.co/(?:e|f)/(?P<id>[^\/$]+)(?:\/|$)'    
     _EMBED_REGEX = [r'<iframe[^>]+?src=([\"\'])(?P<url>https?://(www\.)?redload\.co/e/.+?)\1']
     _MAINJS = f'https://redload.co/assets/js/main.min.js' 
@@ -251,7 +265,7 @@ class RedloadIE(BaseloadIE):
 class HighloadIE(BaseloadIE):
 
     _SITE_URL = "https://highload.to"    
-    IE_NAME = 'highload'
+    IE_NAME = 'highload'# type: ignore
     _VALID_URL = r'https?://(?:www\.)?highload.to/(?:e|f)/(?P<id>[^\/$]+)(?:\/|$)'
     _EMBED_REGEX = [r'<iframe[^>]+?src=([\"\'])(?P<url>https?://(www\.)?highload\.to/e/.+?)\1']
     _MAINJS = 'https://highload.to/assets/js/master.js'
@@ -260,7 +274,7 @@ class HighloadIE(BaseloadIE):
 class EmbedoIE(BaseloadIE):
     
     _SITE_URL = "https://embedo.co"
-    IE_NAME = 'embedo'
+    IE_NAME = 'embedo'# type: ignore
     _VALID_URL = r'https?://(?:www\.)?embedo.co/e/(?P<id>[^\/$]+)(?:\/|$)'
     _EMBED_REGEX = [r'<iframe[^>]+?src=([\"\'])(?P<url>https?://(www\.)?embedo\.co/e/.+?)\1']
     _MAINJS = 'https://embedo.co/assets/js/master.js'
