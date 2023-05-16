9864547246137385da7ddf90260257b511fdf7c4
atgarcia78 <a.tgarc@gmail.com>
2023-05-16 22:33:50 +0200

    upt

--- yt-dlp.cmd
+++ yt-dlp.cmd
@@ -1,1 +1,1 @@
-@py -Wdefault -Xdev "%~dp0yt_dlp\__main__.py" %*
+@py -Walways -Xdev "%~dp0yt_dlp\__main__.py" %*
--- yt-dlp.sh
+++ yt-dlp.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-exec "${PYTHON:-python3}" -Wdefault -Xdev "$(dirname "$(realpath "$0")")/yt_dlp/__main__.py" "$@"
+exec "${PYTHON:-python3}" -Walways -Xdev "$(dirname "$(realpath "$0")")/yt_dlp/__main__.py" "$@"
