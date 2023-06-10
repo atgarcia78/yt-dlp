d136c66585ef86dbc3a3ecbe7d4cd45ec106b8ef
atgarcia78 <a.tgarc@gmail.com>
2023-06-11 00:19:51 +0200

    2023-06-11
    Signed-off-by: atgarcia78 <a.tgarc@gmail.com>

--- yt_dlp/extractor/bbc.py
+++ yt_dlp/extractor/bbc.py
@@ -95,7 +95,6 @@ class BBCBaseIE(InfoExtractor):
         except Exception:
             pass
 
-
     @dec_on_exception
     @limiter.ratelimit("bbc", delay=True)
     def _download_json(self, *args, **kwargs):
