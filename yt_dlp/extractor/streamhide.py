6c46fe626a2653292bbbba694c80ef1e93546bcf
atgarcia78 <a.tgarc@gmail.com>
2023-06-20 06:06:14 +0200

    2023-06-20
    Signed-off-by: atgarcia78 <a.tgarc@gmail.com>

--- yt_dlp/extractor/streamhide.py
+++ yt_dlp/extractor/streamhide.py
@@ -11,7 +11,6 @@ from .commonwebdriver import (
 )
 from ..utils import (
     ExtractorError,
-    decode_packed_codes,
     get_element_text_and_html_by_tag,
     sanitize_filename,
     try_call,
@@ -50,55 +49,49 @@ class StreamHideIE(SeleniumInfoExtractor):
 
     def _get_entry(self, url, **kwargs):
 
-        videoid, dom = try_get(re.search(self._VALID_URL, url), lambda x: x.group('id', 'domain'))  # type: ignore
-        _wurl = f"https://{dom}/e/{videoid}"
-        webpage = try_get(self._send_request(_wurl), lambda x: html.unescape(re.sub('[\t\n]', '', x.text)))
+        try:
 
-        if not webpage:
-            raise ExtractorError("no webpage")
+            videoid, dom = try_get(re.search(self._VALID_URL, url), lambda x: x.group('id', 'domain') if x else (None, None))  # type: ignore
+            _wurl = f"https://{dom}/w/{videoid}"
+            webpage = try_get(self._send_request(_wurl), lambda x: html.unescape(re.sub('[\t\n]', '', x.text)))
 
-        unpacked = decode_packed_codes(webpage)
-
-        m3u8_url = try_get(re.search(r'file:"(?P<url>[^"]+)"', unpacked), lambda x: x.group('url'))
-
-        if not m3u8_url:
-            raise ExtractorError("couldnt find m3u8 url")
-
-        _headers = {'Origin': f"https://{dom}", 'Referer': f"https://{dom}/"}
-
-        formats, subtitles = self._extract_m3u8_formats_and_subtitles(m3u8_url, videoid, ext="mp4", entry_protocol='m3u8_native', m3u8_id="hls", headers=_headers)
-
-        if not formats:
-            raise ExtractorError(f"[{url}] Couldnt find any video format")
-
-        for _format in formats:
-            _format.update({'http_headers': _headers})
-
-        _vurl = f"https://{dom}/w/{videoid}"
-        webpage = try_get(self._send_request(_vurl), lambda x: html.unescape(re.sub('[\t\n]', '', x.text)))
-        if not webpage:
-            _vurl = f"https://{dom}/d/{videoid}"
-            webpage = try_get(self._send_request(_vurl), lambda x: html.unescape(re.sub('[\t\n]', '', x.text)))
             if not webpage:
-                raise ExtractorError("no webpage d/w")
-            title = try_get(try_call(lambda: get_element_text_and_html_by_tag('h4', webpage)[0]), lambda x: re.sub(r'(\s+)?download(\s+)?|\.mp4|\.mkv', '', x, flags=re.IGNORECASE).strip())
-        else:
+                raise ExtractorError("no webpage")
+
+            m3u8_url = try_get(re.search(r'file:"(?P<url>[^"]+)"', webpage), lambda x: x.group('url') if x else None)
+
+            if not m3u8_url:
+                raise ExtractorError("couldnt find m3u8 url")
+
+            _headers = {'Origin': f"https://{dom}", 'Referer': f"https://{dom}/"}
+
+            formats, subtitles = self._extract_m3u8_formats_and_subtitles(m3u8_url, videoid, ext="mp4", entry_protocol='m3u8_native', m3u8_id="hls", headers=_headers)
+
+            if not formats:
+                raise ExtractorError(f"[{url}] Couldnt find any video format")
+
+            for _format in formats:
+                _format.update({'http_headers': _headers})
+
             title = try_get(try_call(lambda: get_element_text_and_html_by_tag('h1', webpage)[0]), lambda x: re.sub(r'(\s+)?download(\s+)?|\.mp4|\.mkv', '', x, flags=re.IGNORECASE).strip())
 
-        _entry = {
-            "id": videoid,
-            "title": sanitize_filename(title, restricted=True).replace("Watch_", ""),
-            "formats": formats,
-            "subtitles": subtitles,
-            "webpage_url": _wurl,
-            "ext": "mp4"}
+            _entry = {
+                "id": videoid,
+                "title": sanitize_filename(title, restricted=True).replace("Watch_", ""),
+                "formats": formats,
+                "subtitles": subtitles,
+                "webpage_url": _wurl,
+                "ext": "mp4"}
 
-        try:
-            _entry.update({'duration': self._extract_m3u8_vod_duration(formats[0]['url'], videoid, headers=formats[0].get('http_headers', {}))})
+            try:
+                _entry.update({'duration': self._extract_m3u8_vod_duration(formats[0]['url'], videoid, headers=formats[0].get('http_headers', {}))})
+            except Exception as e:
+                self.logger_info(f"error trying to get vod {repr(e)}")
+
+            return _entry
+
         except Exception as e:
-            self.logger_info(f"error trying to get vod {repr(e)}")
-
-        return _entry
+            self.LOGGER.exception(repr(e))
 
     def _real_extract(self, url):
 
